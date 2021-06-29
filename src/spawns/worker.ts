45da893f87b88111dc9db2e465e14cd9b981c2f3
Sugar_F0x <sugarfox.dev@gmail.com>
2021-06-29 10:54:57 +0300

    :recycle: Code consistency cleanup

--- src/spawns/worker.ts
+++ src/spawns/worker.ts
@@ -1,20 +1,20 @@
 import { displayTextPrompt, getAvailableName } from "utils"
 
-const WORKERS_REQUIRED = 3
-const WORKER_BASE = [WORK, MOVE, CARRY]
+const CREEPS_REQUIRED = 3
+const CREEP_BASE = [WORK, MOVE, CARRY]
 
 export const workerSpawnLogic = (room: Room) => {
   const spawn = room.find(FIND_MY_SPAWNS)[0]
   if (!spawn) return
   if (spawn.spawning) { displaySpawningText(spawn); return }
-  if (spawn.spawnCreep(WORKER_BASE, 'dry', { dryRun: true }) !== OK) return
+  if (spawn.spawnCreep(CREEP_BASE, 'dry', { dryRun: true }) !== OK) return
 
-  const workers = room.find(FIND_CREEPS, { filter: { memory: { role: 'worker' } } })
-  if (workers.length >= WORKERS_REQUIRED) return
+  const creeps = room.find(FIND_CREEPS, { filter: { memory: { role: 'worker' } } })
+  if (creeps.length >= CREEPS_REQUIRED) return
 
-  const newWorkerName = getAvailableName(workers, 'Worker')
+  const newCreepName = getAvailableName(creeps, 'Worker')
   const memoryOptions = { memory: { role: 'worker', room: room.name, working: false } }
-  spawn.spawnCreep(WORKER_BASE, newWorkerName, memoryOptions)
+  spawn.spawnCreep(CREEP_BASE, newCreepName, memoryOptions)
   displaySpawningText(spawn)
 }
 
