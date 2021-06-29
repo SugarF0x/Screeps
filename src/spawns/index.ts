01a35d03cf5edfa5a2e842578c2d2491f2bd78d6
Sugar_F0x <sugarfox.dev@gmail.com>
2021-06-29 11:41:46 +0300

    :hammer_and_wrench: Fix spawn priority
    Workers are to be spawned in first
    The last spawn logic gets prioritized

--- src/spawns/index.ts
+++ src/spawns/index.ts
@@ -7,7 +7,7 @@ export const spawn = () => {
   for (const roomName in Game.rooms) {
     const room = Game.rooms[roomName]
 
-    workerSpawnLogic(room)
     upgraderSpawnLogic(room)
+    workerSpawnLogic(room)
   }
 }
